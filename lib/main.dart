47bac42769456dcb26c9798a35a0755db6d6aa52
Shmyaks <maxonic10@list.ru>
2021-08-19 01:45:35 +0300

    Update News. (New version)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:news_page/views/homepage.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_page/presentation/bloc/news_bloc.dart';
+import 'package:news_page/presentation/bloc/news_bloc_event.dart';
+import 'package:news_page/presentation/pages/news_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,10 +13,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.white,
-      ),
-      home: HomePage(),
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color(0xff181A20),
+            title: Text("Новости"),
+          ),
+          backgroundColor: Color(0xff181A20),
+          body: BlocProvider(
+              create: (context) => NewsBloc()..add(NewsInitital()),
+              child: NewsPage())),
     );
   }
 }
